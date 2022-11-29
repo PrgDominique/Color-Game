11a8839f3b30ec81daa6ddc059d63ee6bf6481a2
PrgDominique <dominiquesimbillo10@gmail.com>
2022-11-29 16:56:06 +0800

    added random guess color name

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
- window.onload =function colorGame(){
-    
+function capFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateColor() {
+  const color1 = ["Pink", "Blue", "Orange", "Red", "Black", "LightBlue"];
+
+  var color = capFirst(color1[getRandomInt(0, color1.length + 1)]);
+
+  return color;
+}
+
+document.getElementById("color-guess").innerHTML = generateColor();
